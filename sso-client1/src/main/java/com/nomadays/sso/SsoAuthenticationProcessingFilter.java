f16d1a7e72831ff4fc09a3fea037000bb8508f4f
beku <beku2009@gmail.com>
2016-09-07 10:41:11 +0900

    Cookie expiry set

--- sso-client1/src/main/java/com/nomadays/sso/SsoAuthenticationProcessingFilter.java
+++ sso-client1/src/main/java/com/nomadays/sso/SsoAuthenticationProcessingFilter.java
@@ -1,6 +1,7 @@
 package com.nomadays.sso;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
@@ -14,20 +15,20 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.session.Session;
+import org.springframework.session.ExpiringSession;
 import org.springframework.session.SessionRepository;
 
 public class SsoAuthenticationProcessingFilter extends AbstractAuthenticationProcessingFilter {
 	
 	private Logger logger = LoggerFactory.getLogger(getClass());
-	private SessionRepository<Session> sessionRepository;
+	private SessionRepository<ExpiringSession> sessionRepository;
 	
 	public final String TOKEN_PARAM = "token";
 
 	@SuppressWarnings("unchecked")
-	public <S extends Session> SsoAuthenticationProcessingFilter(SessionRepository<S> sessionRepository) {
+	public <S extends ExpiringSession> SsoAuthenticationProcessingFilter(SessionRepository<S> sessionRepository) {
 		super(new AntPathRequestMatcher("/login_callback"));
-		this.sessionRepository = (SessionRepository<Session>) sessionRepository;
+		this.sessionRepository = (SessionRepository<ExpiringSession>) sessionRepository;
 	}
 
 	@Override
@@ -37,14 +38,20 @@ public class SsoAuthenticationProcessingFilter extends AbstractAuthenticationPro
 		
 		String sessionId = request.getParameter(TOKEN_PARAM);
 		if(sessionId != null){
-			Session session = sessionRepository.getSession(sessionId);
+			ExpiringSession session = sessionRepository.getSession(sessionId);
 			if(session != null){
 				SecurityContext securityContext = session.getAttribute("SPRING_SECURITY_CONTEXT");
 				logger.debug("session {}", securityContext.getAuthentication());
 				logger.debug("authentication manager {}", this.getAuthenticationManager());
 				
+				Long expiryInMilliSeconds = session.getCreationTime() + session.getMaxInactiveIntervalInSeconds() * 1000;
 				
-				response.addCookie(new Cookie("SESSION", sessionId));
+				
+				Long maxAge = (new Date().getTime() - expiryInMilliSeconds)/1000;
+				logger.debug("maxAge {}", maxAge.toString());
+				Cookie cookie = new Cookie("SESSION", sessionId);
+				cookie.setMaxAge(Integer.parseInt(maxAge.toString()));
+				response.addCookie(cookie);
 				return securityContext.getAuthentication();
 			}
 		}
